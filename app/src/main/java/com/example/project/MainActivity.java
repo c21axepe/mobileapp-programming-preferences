973cd2bdebe92daa8defdce092d13c2ad15c0107
Axel <c21axepe@student.his.se>
2022-05-04 15:03:26 +0200

    Skapade en till activity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text_name;
+    EditText edit_name;
+    SharedPreferences preferences;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +23,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
 
+        text_name = findViewById(R.id.text_name);
+
+        preferences = getSharedPreferences("Axel", MODE_PRIVATE);
+
+        button = findViewById(R.id.prefButton);
+        edit_name = findViewById(R.id.preftext);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("name", edit_name.getText().toString());
+                editor.apply();
+            }
+        });
+
+
+        }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        String name = preferences.getString("name", "cant get name");
+        text_name.setText(name);
+    }
 }
